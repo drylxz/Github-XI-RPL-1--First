691f51deee2142601fcb600bdb67a77dc5109f73
drylxz <adrian_238@smkassalaambandung.sch.id>
2022-01-18 11:01:27 +0700

    pesan

---
+++ dowhile.php
@@ -0,0 +1,28 @@
+<?php
+echo "while<br>";
+echo "<br>";
+echo "contoh 1<br>";
+$x = 1;
+do {
+    echo "Do While: $x <br>";
+    $x++;
+} while($x <= 10);
+echo "<br>";
+echo "contoh 2<br>";
+$i=123;
+do
+{
+  echo "$i";
+  echo " akan tampil di browser ini<br>";
+  $i=$i+1;
+} while ($i <= 10);
+echo "<br>";
+echo "contoh 3<br>";
+$i=1;
+do
+{
+  echo "$i";
+  echo "<br />";
+  $i=$i+1;
+} while ($i <= 10);
+?> 
